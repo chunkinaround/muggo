7d4f681efb41cc4b5362eab0c1045249208deb14
chunkinaround <36927478+chunkinaround@users.noreply.github.com>
2021-10-14 14:51:15 -0500

    Update muggo.js

--- scripts/muggo.js
+++ scripts/muggo.js
@@ -1,1 +1,1 @@
-visitUrl('curse.php?action=use&whichitem=3280&targetplayer=1515124&texta="Hey"&textb="this is"&textc="a muggo")';
+visitUrl('curse.php?action=use&whichitem=3280&targetplayer=1515124&texta="Hey"&textb="this is"&textc="a muggo"');
